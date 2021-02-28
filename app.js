4e3763007e433eef92e48513fd01958526c86309
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-02-27 22:34:01 -0600

    added User.js & Todo.js model

--- app.js
+++ app.js
@@ -19,14 +19,33 @@ mongoose.connect(
 // const userRouter = require("./routes/User");
 // app.use("/user", userRouter);
 
+const User = require("./models/User");
+
+// const userInput = {
+//   username: "mahmoudosman",
+//   password: "12345678910",
+//   role: "admin",
+// };
+
+// const user = new User(userInput);
+
+// user.save((err, document) => {
+//   if (err) console.log(err);
+//   console.log(document);
+// });
+
 //Check if the connection variable //3
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.listen(9000, () => {
-  console.log("express server started");
-});
+// app.listen(5050, () => {
+//   console.log("express server started");
+// });
+const port = process.env.PORT || 5050;
+const server = app.listen(port, () =>
+  console.log("Serve started at http://localhost:5050")
+);
 
 //Show/handle the server error in better way
 process.on("unhandledRejection", (err, promise) => {
