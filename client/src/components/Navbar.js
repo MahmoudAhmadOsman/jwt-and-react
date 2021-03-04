f9327a844e0a4a3cfec6d07c774af0d484802643
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-03-04 08:55:37 -0600

    added dynamic navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,89 @@
-import React from "react";
-
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-function Navbar() {
+import AuthService from "../Services/AuthService";
+import { AuthContext } from "../Context/AuthContext";
+const Navbar = (props) => {
+  //Pull out the context
+  const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(
+    AuthContext
+  );
+
+  //Logout
+  const onClickLogoutHandler = () => {
+    AuthService.logout().then((data) => {
+      if (data.success) {
+        setUser(data.user);
+        setIsAuthenticated(false);
+      }
+    });
+  };
+
+  //Unauthenticated NavBar - show this if not logged in
+  const unauthenticatedNavBar = () => {
+    return (
+      <>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <Link to="/">
+            <li className="nav-item nav-link">
+              {" "}
+              Home <span className="sr-only">(current)</span>
+            </li>
+          </Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </>
+    );
+  };
+
+  //Authenticated NavBar - show this when user is logged in
+  const authenticatedNavBar = () => {
+    return (
+      <>
+        <Link to="/">
+          <li className="nav-item nav-link active">Home</li>
+        </Link>
+        <Link to="/todos">
+          <li className="nav-item nav-link">Todos</li>
+        </Link>
+        {/* Display pages based on user role */}
+        {user.role === "admin" ? (
+          <Link to="/admin">
+            <li className="nav-item nav-link">Admin</li>
+          </Link>
+        ) : null}
+
+        {/* Logout link Or button */}
+        <button
+          type="button"
+          className="btn btn-link nav-item nav-link"
+          onClick={onClickLogoutHandler}
+        >
+          Logout
+        </button>
+      </>
+    );
+  };
+
   return (
     <section className="main_navigation">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -20,8 +102,11 @@ function Navbar() {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
+          {/* <ul className="navbar-nav mr-auto mt-2 mt-lg-0">  */}
+
+          <ul className="navbar-nav">
+            {!isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar()}
+            {/* <li className="nav-item active">
               <a className="nav-link" href="/">
                 Home <span className="sr-only">(current)</span>
               </a>
@@ -35,9 +120,9 @@ function Navbar() {
               <Link className="nav-link" to="#">
                 Contact
               </Link>
-            </li>
+            </li> */}
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          {/* <form className="form-inline my-2 my-lg-0">
             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
               <li className="nav-item">
                 <Link className="nav-link" to="/register">
@@ -50,11 +135,11 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
-          </form>
+          </form> */}
         </div>
       </nav>
     </section>
   );
-}
+};
 
 export default Navbar;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,7 +4,52 @@ export default function Register() {
   return (
     <section className="register">
       <div className="container">
-        <h1>Register</h1> <hr />
+        <h1 className="text-info">Register</h1> <hr />
+        <div className="row">
+          <div className="col-md-8 mx-auto">
+            <form action="">
+              <div className="form-group">
+                <label htmlFor="username" className="font-weight-bold">
+                  User Name
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="font-weight-bold">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Enter your email address"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="font-weight-bold">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-outline-info btn-lg font-weight-bold"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
