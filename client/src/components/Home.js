cb82596900c0961d6276adc7e3a41936c6f5152b
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-03-04 09:12:59 -0600

    added dynamic navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home_page">
-      <div className="container">
-        <h1>Home</h1> <hr />
+      <div class="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+          <h1 className="text-info">
+            React & Jwt Authentication & Authorization
+          </h1>
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="#" className="btn btn-outline-info btn-lg mr-3 mt-5">
+                Learn More
+              </Link>
+              <Link to="#" className="btn btn-outline-success btn-lg mt-5">
+                Learn More
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
