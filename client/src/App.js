c22b27be1261eb57950291e52bcbdbd1335035be
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-03-09 22:52:22 -0600

    added register, login & message components

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-    return (
-        <section className="app">
+  return (
+    <section className="app">
       <Router>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
         </Switch>
       </Router>
     </section>
-    );
+  );
 }
 
 export default App;
---
+++ client/src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <div className="container">
+        <h1 className="text-info">Contact Us</h1> <hr />
+        <div className="row">
+          <div className="col-md-10">
+            <p>Comming Soon!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
